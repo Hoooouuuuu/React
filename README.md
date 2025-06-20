abd4893c4bb355a8a79ef6adb0d0b0c92379dac8
Hoooouuuuu <whdgh91@gmail.com>
2025-06-20 12:18:41 +0900

    feat: 네브바 추가 및 글 목록/작성 페이지 이동

--- README.md
+++ README.md
@@ -9,6 +9,17 @@
 - CSS (또는 Tailwind/Styled-components로 확장 가능)
 - Git, GitHub
 
+## 🐙 Git 사용 툴
+- Hoooouuuuu Branch Test 업로드
+- Main으로 최종 병합
+
+-- feat: 새로운 기능 추가, 기존의 기능을 수정
+-- fix: 버그 및 에러에 대한 수정
+-- chore: 기타 수정 ex).gitignore / .env
+-- docs: 주석 수정
+-- test: 코드 추가 및 수정
+-- remove: 파일을 삭제한 경우
+
 ## 🚀 주요 기능 (예정)
 - 글 목록 보기
 - 글 작성
--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 import PostListPage from './pages/PostListPage';
 import PostWritePage from './pages/PostWritePaga';
 
 function App() {
   return (
-    <div className='App'>
-      <PostWritePage />
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <Link to={'/'}>글 목록</Link> | <Link to={'/write'}>글 작성</Link>
+        </nav>
+        <hr />
+
+        <Routes>
+          <Route path='/' element={<PostListPage />} />
+          <Route path='/write' element={<PostWritePage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
